d6363845c68d1a62266c0afaf747eec94027a13d
Kiel Davignon <kieldavignon@outlook.com>
2026-01-23 16:59:03 -0500

    added bulk remove user from groups

--- Groups/New-EzGroupMembership.ps1
+++ Groups/New-EzGroupMembership.ps1
@@ -14,12 +14,84 @@ $Confirmed = $false
         
         try {
 
-            $P = Read-Host "Please add in each user with a corresponding group. When done, save the csv and exit. Type C to DryRun, Type Y when finished (or type Q to quit)"
+            $P = Read-Host "Please add in each user with a corresponding group. When done, save the csv and exit. Type C to DryRun, Type A to add users to groups, Type D to remove users from groups. when finished (or type Q to quit)"
 
             switch ($P.ToUpper()) {
-                "Y" {
+                
+                "A" {
+
                     Write-Host "Proceeding with script..." -ForegroundColor Green
+
+                    $userGroupAdd = Import-Csv -Path "$env:USERPROFILE\Documents\EntraController\Groups\bulkUserGroupAdd.csv"
+
+                    foreach ($Item in $userGroupAdd) {
+    
+                        try {
+                        # Retrieve group Id of each group
+                        $groupId = Get-MgGroup -Filter "displayName eq '$($Item.groupName.Trim())'" -ErrorAction Stop
+                        $groupId = $groupId.Id
+
+                        #retrieve directory object ID of each user
+                        $userId = Get-MgUserByUserPrincipalName -UserPrincipalName $Item.userPrincipalName.Trim() -ErrorAction stop
+                        $userId = $userId.Id
+                        }
+                        catch {
+                            Write-Host -ForegroundColor Red "An error occurred while processing user '$($Item.userPrincipalName)' or group '$($Item.groupName)': $_"
+                        }
+
+                        #add each user to each group
+                        try {
+
+                            New-MgGroupMember -GroupId $groupId -DirectoryObjectId $userId -ErrorAction Stop
+                            Write-Host -ForegroundColor Green "[ADD] Adding user $($Item.userPrincipalName) to group $($Item.groupName)"
+
+                        }
+                        catch {
+                            Write-Host -ForegroundColor Red "Error adding user $($Item.userPrincipalName) to group $($Item.groupName): $_"
+                        }
+    
+    
+   
+                    }
                     $Confirmed = $true
+                }
+                "D" {
+
+                    Write-Host "Proceeding with script..." -ForegroundColor Green
+
+                    $userGroupAdd = Import-Csv -Path "$env:USERPROFILE\Documents\EntraController\Groups\bulkUserGroupAdd.csv"
+
+                    foreach ($Item in $userGroupAdd) {
+    
+                        try {
+                        # Retrieve group Id of each group
+                        $groupId = Get-MgGroup -Filter "displayName eq '$($Item.groupName.Trim())'" -ErrorAction Stop
+                        $groupId = $groupId.Id
+
+                        #retrieve directory object ID of each user
+                        $userId = Get-MgUserByUserPrincipalName -UserPrincipalName $Item.userPrincipalName.Trim() -ErrorAction stop
+                        $userId = $userId.Id
+                        }
+                        catch {
+                            Write-Host -ForegroundColor Red "An error occurred while processing user '$($Item.userPrincipalName)' or group '$($Item.groupName)': $_"
+                        }
+
+                        #add each user to each group
+                        try {
+
+                            Remove-MgGroupMemberByRef -GroupId $groupId -DirectoryObjectId $userId -ErrorAction Stop
+                            Write-Host -ForegroundColor Yellow "[REMOVE] Removing user $($Item.userPrincipalName) from group $($Item.groupName)"
+                            
+                        }
+                        catch {
+                            Write-Host -ForegroundColor Red "Error Removing user $($Item.userPrincipalName) to group $($Item.groupName): $_"
+                        }
+    
+    
+   
+                    }
+                    $Confirmed = $true
+                
                 }
                 "Q" {
                     Write-Host "Exiting script. Goodbye!" -ForegroundColor Yellow
@@ -28,7 +100,7 @@ $Confirmed = $false
                 "C"{
                     $userGroupAdd = Import-Csv -Path "$env:USERPROFILE\Documents\EntraController\Groups\bulkUserGroupAdd.csv"
                     foreach($Item in $userGroupAdd){
-                            write-host -ForegroundColor Cyan "[CONFIRM] This will add USER: $($Item.userPrincipalName) the GROUP: $($Item.groupName) "
+                            write-host -ForegroundColor Cyan "[CONFIRM] This will apply to USER: $($Item.userPrincipalName) and to the GROUP: $($Item.groupName) "
                         }
                         Write-Host "Dry Run completed. No changes were made." -ForegroundColor Yellow
                     }   
@@ -44,38 +116,6 @@ $Confirmed = $false
         }
     }
 
-
-$userGroupAdd = Import-Csv -Path "$env:USERPROFILE\Documents\EntraController\Groups\bulkUserGroupAdd.csv"
-
-
-foreach ($Item in $userGroupAdd) {
-    
-    try {
-    # Retrieve group Id of each group
-    $groupId = Get-MgGroup -Filter "displayName eq '$($Item.groupName.Trim())'" -ErrorAction Stop
-    $groupId = $groupId.Id
-
-    #retrieve directory object ID of each user
-    $userId = Get-MgUserByUserPrincipalName -UserPrincipalName $Item.userPrincipalName.Trim() -ErrorAction stop
-    $userId = $userId.Id
-    }
-    catch {
-        Write-Host -ForegroundColor Red "An error occurred while processing user '$($Item.userPrincipalName)' or group '$($Item.groupName)': $_"
-    }
-
-    #add each user to each group
-    try {
-        
-        New-MgGroupMember -GroupId $groupId -DirectoryObjectId $userId -ErrorAction Stop
-        Write-Host -ForegroundColor Green "Adding user $($Item.userPrincipalName) to group $($Item.groupName)"
-    }
-    catch {
-        Write-Host -ForegroundColor Red "Error adding user $($Item.userPrincipalName) to group $($Item.groupName): $_"
-    }
-    
-    
-   
-}
 
 $ErrorActionPreference = 'Continue'
 
